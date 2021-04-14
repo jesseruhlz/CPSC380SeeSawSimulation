b362e945b35825595450edc7335832bc6fdb3358
super <super@host.docker.internal>
2021-04-13 19:10:01 -0700

    Created an outline for the threads and semaphores, need to figure out what to put in the thread and semaphore functions.

--- src/seesawsimulation/SeeSawSimulation.java
+++ src/seesawsimulation/SeeSawSimulation.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package seesawsimulation;
+import java.util.concurrent.*; //this is needed to use semaphore in java
+import java.io.*;
+import java.util.*;
 
 /**
- *
- * @author super
+ * Name: Jesse Ruhl
+ * Chapman ID: 
+ * Course: CPSC 380
  */
-public class SeeSawSimulation {
 
-    /**
-     * @param args the command line arguments
-     */
+public class SeeSawSimulation {
+    
+    static double fredHeight = 1.0; // assuming at time 0 person A at low end
+    static double wilmaHeight = 7.0; // assuming at time 0 person B at high end
+    static boolean fredUp = true;
+    static int time = -0;
+
+    //main function
     public static void main(String[] args) {
         // TODO code application logic here
+        //Thread 1 for Fread created here
+        // Thread 2 for Wilma created here
+        
+        //try to start each thread and join each thread
+    }
+    
+    // calculate the height at each interval
+    public static void calculateHeight(Semaphore sem){
+        
+    }
+    
+    // will ouput the updates on each interval
+    public static void outputSema(Semaphore outSem){
+        
+    }
+    
+    class FredSee extends Thread implements Runnable{
+        
+    }
+    
+    class WilmaSaw extends Thread implements Runnable{
+        
     }
     
 }
